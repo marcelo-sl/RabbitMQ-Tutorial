50db640b4588e3f0354679446a5cd46697510db9
marcelo-sl <mar.sousa2061@gmail.com>
2022-06-13 10:21:24 -0300

    RabbitMQ Tutorial

---
+++ src/RabbitMq.Producer/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "hello",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    int counter = 0;
+
+    while (true)
+    {
+        string message = $"Hello World {counter++}!";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: "",
+                         routingKey: "hello",
+                         basicProperties: null,
+                         body: body);
+        Console.WriteLine(" [x] Sent {0}", message);
+
+        Task.Delay(200).Wait();
+    }
+}
